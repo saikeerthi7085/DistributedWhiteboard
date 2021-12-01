fa9501d471d14bef7bcee0d37551a71b3c50ac25
Sai Keerthi Maddineni <saikeerthimaddineni@gmail.com>
2021-12-01 12:37:05 +0000

    first commit

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DistributedWhiteBoard.MvcApplication" Language="C#" %>
---
+++ Startup1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(DistributedWhiteBoard.Startup1))]
+
+
+namespace DistributedWhiteBoard
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
